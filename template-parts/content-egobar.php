ed3967b117e610c7f408131fc1dcd2d76325bd2d
Middi <richard@richardmiddleton.me>
2020-01-21 18:22:17 +0100

    startiing how it works section

--- template-parts/content-egobar.php
+++ template-parts/content-egobar.php
@@ -10,7 +10,9 @@
 ?>
 
 <section class="egobar">
-    <h3 class="display-h3 h3-underline underline-pink text-center">We've Been Featured On</h3>
+    <div class="title-container">
+        <h3 class="display-h3 h3-underline underline-green">We've Been Featured On</h3>
+    </div>
     <div class="carousel" id="carousel">
         <img id="ego-1" src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/img/egobar.png ?>" " alt="image" />
 
