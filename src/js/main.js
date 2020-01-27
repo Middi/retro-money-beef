1f69dc44c40b0c22bb4d140ef1c61ecb7cc5868d
Middi <richard@richardmiddleton.me>
2020-01-27 14:35:00 +0100

    small changes

--- src/js/main.js
+++ src/js/main.js
@@ -76,24 +76,6 @@ jQuery('#user-form').ajaxForm({
 	}
 });
 
-// var getUrlParameter = function getUrlParameter(sParam) {
-// 	var sPageURL = window.location.search.substring(1),
-// 		sURLVariables = sPageURL.split('&'),
-// 		sParameterName,
-// 		i;
-// 	for (i = 0; i < sURLVariables.length; i++) {
-// 		sParameterName = sURLVariables[i].split('=');
-// 		if (sParameterName[0] === sParam) {
-// 			return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-// 		}
-// 	}
-// };
-// jQuery( document ).ready(function() {
-// 	console.log( "ready!" );
-// 	jQuery('#acf-field_5df80db3d9ca0').val(getUrlParameter('customer'));
-// });
-
-
 // ===========================
 // Egobar
 // ===========================
